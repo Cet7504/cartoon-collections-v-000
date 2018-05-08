c7e7c325bbda185dd746a0a9c8198e6c671d2cb0
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-05-08 00:13:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |calls| calls.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  snacks.find do |cheese|
+    cheese_types.include? (cheese)
